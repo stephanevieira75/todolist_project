fda6dbf988472e69c20bd873da2b7f34280fae89
stephane <stephane.vieira75@outlook.fr>
2020-02-13 00:28:56 +0100

    add: InsertData function in module

--- Crud.js
+++ Crud.js
@@ -1,2 +1,28 @@
 // Import database module
 const Data = require('./Database');
+
+// Create
+let insertData = () => {
+    const mongoDatabase = require('mongodb');
+    const MongoClient = mongoDatabase.MongoClient;
+
+    const client = new MongoClient(Data.url);
+
+    client.connect()
+    .then(()=> {
+        const db = client.db(Data.name);
+        const collection = db.collection(Data.collectionName);
+        return collection;
+    })
+    .then((collection) => {
+        return collection.insertMany(Data.documents);
+    })
+    .then((response) => {
+        console.log('Inserted: ', response.result.n, 'documents');
+        console.log('documents', response.ops);  
+    })
+    .then(() => client.close())
+    .catch((e) => console.log(e));
+};
+
+module.exports = { insertData }
