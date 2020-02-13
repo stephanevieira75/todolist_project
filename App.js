f1521041a7556a7e26df5439ac6253094f33d0c4
stephane <stephane.vieira75@outlook.fr>
2020-02-13 11:37:55 +0100

    mend

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const initServer = require('./Server');
 const crud = require('./Crud');
 
 //crud.setTask('task power lorem ipsum', false);
-//crud.updateTask("function bil() should take param", true);
+//crud.updateTask("5e441b64c9cd9d276972e399", "destroy bobo", true);
 //crud.getTasks();
 //crud.deleteTask("function bil() should take param");
 //initServer();
