dccb658d1cc046acdfc5e12a0e25a8df495dd21e
stephane <stephane.vieira75@outlook.fr>
2020-02-13 06:48:22 +0100

    add: getTasks method from Crud module

--- App.js
+++ App.js
@@ -3,5 +3,6 @@ const initServer = require('./Server');
 const crud = require('./Crud');
 
 //crud.setTasks('task power lorem ipsum', false);
+//crud.updateTasks("function bil() should take param", true);
 //crud.getTasks();
 //initServer();
