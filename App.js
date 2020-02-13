6b1d987c8babe76f5ab8df374d7d17d443957ac0
stephane <stephane.vieira75@outlook.fr>
2020-02-13 06:58:38 +0100

    add: deleteTask method from Crud module

--- App.js
+++ App.js
@@ -2,7 +2,8 @@
 const initServer = require('./Server');
 const crud = require('./Crud');
 
-//crud.setTasks('task power lorem ipsum', false);
-//crud.updateTasks("function bil() should take param", true);
+//crud.setTask('task power lorem ipsum', false);
+//crud.updateTask("function bil() should take param", true);
 //crud.getTasks();
+//crud.deleteTask("function bil() should take param");
 //initServer();
